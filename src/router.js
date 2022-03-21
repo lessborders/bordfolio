2b8f837ec0da3c5183227faec83453c41a8e92b5
Billie Bell <billie.bell@net-up.de>
2022-03-21 02:40:43 +0100

    updates

--- src/router.js
+++ src/router.js
@@ -9,12 +9,52 @@ export const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/auth/signin",
+      name: "login",
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     }
   ],
 });
 
 router.beforeEach((to, from, next) => {
-  console.log({to, from});
-  window.scrollTo(0, 0)
-  next();
+  let toFullPath = new URL(to.fullPath, window.location.href).href
+  let overridePrev = false
+  if(to.name == "login") {
+    toFullPath = new URL(from.fullPath, window.location.href).href
+    overridePrev = true
+  }
+  const fromFullPath = new URL(from.fullPath, window.location.href).href
+  let newUrl = null
+  // requires auth, check if logged in
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // if not authorized, redirect to login page.
+    if (!store.state.isLoggedIn) {
+      console.log(store.state.isUserLoggedIn)
+      newUrl = 'https://cloud.lessborders.com/auth/signin/?redirect_url=' + toFullPath
+      if(toFullPath != fromFullPath || overridePrev) {
+        newUrl += '&previous_url=' + fromFullPath
+      }
+      window.location.href = newUrl
+    } else {
+      next() // go to wherever I'm going
+      window.scrollTo(0, 0)
+    }
+  } else if (to.matched.some(record => record.meta.hideForAuth)) {
+    // if authorized, redirect to home page.
+    if (store.state.isLoggedIn) {
+      next({ name: 'home' })
+      window.scrollTo(0, 0)
+    } else {
+      next() // go to wherever I'm going
+      window.scrollTo(0, 0)
+    }
+  } else {
+    next()
+    window.scrollTo(0, 0)
+  }
 });
