96b5bfacd04d082138f2e74d9a4f9adf3ef71276
Billie Bell <billie.bell@net-up.de>
2022-04-01 00:35:02 +0200

    initial commit

---
+++ layouts/page.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <slot name="app-before" />
+    <div id="app-before"></div>
+    <div class="d-flex flex-column m-vh-100">
+      <slot name="header">
+        <PageNavbar />
+      </slot>
+      <main id="page" class="d-flex col mx-auto">
+        <slot />
+      </main>
+      <slot name="footer">
+        <PageFooter />
+      </slot>
+    </div>
+    <slot name="app-after" />
+    <div id="app-after"></div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+
+if (!route.meta.title) {
+  route.meta.title = 'Your awesome online portfolio'
+}
+
+console.log(route.meta)
+
+useMeta({
+  title: computed(() => `${route.meta.title} · Bordfolio by Less Borders`),
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content:
+        route.meta.description ||
+        'Create your portfolio website in minutes and share it with your friends or potential employers.',
+    },
+  ],
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+  title: 'Your awesome online portfolio',
+  description:
+    'Create your portfolio website in minutes and share it with your friends or potential employers.',
+})
+</script>
